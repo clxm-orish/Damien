7bc091dd561eaffc7a56da43da1e9bd44d8abeda
clxm-orish <cl.villain@orisha.com>
2025-07-15 15:56:15 +0200

    Fix contact form sender and add cookies page

---
+++ portfolio-damien/app/traitement-des-cookies/page.tsx
@@ -0,0 +1,16 @@
+export default function TraitementCookiesPage() {
+    return (
+        <main className="prose mx-auto p-8">
+            <h1>Traitement des cookies</h1>
+            <p>
+                Ce site utilise des cookies strictement nécessaires à son fonctionnement,
+                notamment pour sécuriser le formulaire de contact. Aucune donnée
+                personnelle n'est exploitée à des fins commerciales.
+            </p>
+            <p>
+                En utilisant le formulaire, vous acceptez que ces cookies soient
+                stockés dans votre navigateur le temps de votre navigation.
+            </p>
+        </main>
+    );
+}
